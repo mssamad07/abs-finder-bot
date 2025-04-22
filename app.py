15ca9abbb0fdf2f4380a7ef8cbfb034ba2d549e3
ᴍɪɢᴜᴇʟ ᴏ’ʜᴀʀᴀ! <anmol0700k@gmail.com>
2025-04-22 17:42:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'TechVJ'
+    return 'ᴍɪɢᴜᴇʟ ᴏ’ʜᴀʀᴀ!'
 
 
 if __name__ == "__main__":
